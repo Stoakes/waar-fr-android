290027416232d2c1f99da8baee96c81644c4dc60
pochon.pierre <pochon.pierre@gmail.com>
2012-06-27 14:35:29 +0000

    webview fonctionnel.

--- WaaR.fr/src/waar/WaaRActivity.java
+++ WaaR.fr/src/waar/WaaRActivity.java
@@ -3,7 +3,9 @@ package waar;
 import fr.waar.android.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class WaaRActivity extends Activity {
@@ -12,24 +14,38 @@ public class WaaRActivity extends Activity {
     private WebView webView;
     private final String mimeType = "text/html";
     private final String encoding = "utf-8";
-    private final String waarUrl = "www.waar.fr";
-    
+    private final String waarUrl = "http://www.waar.fr";
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	try
-    	{
-            super.onCreate(savedInstanceState);
+    		super.onCreate(savedInstanceState);
             setContentView(R.layout.main);
             
             webView = (WebView) findViewById(R.id.WebView);
-           
-            webView.loadData(waarUrl, mimeType, encoding);
-    	}
-    	catch (Exception e)
-    	{
-    		Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG);
-    	}
+            
+            //pageWeb = getPage(waarUrl);
+            webView.setWebViewClient(new myWebViewClient());
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl(waarUrl);
+            //webView.loadData(pageWeb, mimeType, encoding);
+            //webView.loadUrl(waarUrl);
 
     }
+    
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+			webView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private class myWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
 }
